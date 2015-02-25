a3dd041a59b3e033d79fa6a34cd1a59a01824026
Filip Gummesson <filip.gummesson@gmail.com>
2015-02-25 13:51:55 +0100

    new constants

--- constants.h
+++ constants.h
@@ -1,10 +1,24 @@
 #ifndef constants
 
 
-#define K_LENGTH 3
-#define BLOCK_LENGTH 9
-#define OVERLAP 80
-#define Fs 8000
-#define THRESHOLD 0.05
+//Sample rate in Hz
+#define Fs              8000
+
+//The number of samples in each block
+#define BLOCK_LENGTH    160
+
+//Record for this many seconds
+#define REC_TIME        1.5
+
+//How many samples will be copied form previous block into present block
+#define OVERLAP         80
+
+//number of block of one recording.
+#define N_BLOCKS        ( (Fs * REC_TIME)/(BLOCK_LENGTH - OVERLAP)  )
+
+
+
+//Isn´t useful yet
+#define THRESHOLD       0.05
 
 #endif
