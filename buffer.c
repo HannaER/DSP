9dae54cfb6412870c1a76d784df99c85ca43b2e9
Filip Gummesson <filip.gummesson@gmail.com>
2015-02-25 14:54:29 +0100

    new buffer function

---
+++ buffer.c
@@ -0,0 +1,32 @@
+
+static float buffer[BUFFER_LENGTH][BLOCK_LENGTH];
+static int length = 0;
+static int index = 0;
+
+void put(float* block)
+{
+	if (length < 3){
+		length += 1;
+	}
+
+	int i;
+	for(i = 0; i < BLOCK_LENGTH, i++){
+		buffer[index][i] = block[i];
+	}
+
+	index = (index + 1) % BUFFER_LENGTH;
+}
+
+
+int poll(float* blocks){
+	for (int i = 0; i < length; ++i)
+	{
+		for (int j = 0; j < BLOCK_LENGTH; ++j)
+		{
+			blocks[i][j] = buffer[i][j];
+		}
+	}
+
+	return length;
+}
+
---
+++ buffer.h
@@ -0,0 +1,15 @@
+#ifndef
+
+#include "constants.h"
+
+#define BUFFER_LENGTH 		3
+
+
+
+void put(float* block);
+
+void poll(float* blocks);
+
+
+#endif
+
